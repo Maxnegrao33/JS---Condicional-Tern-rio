ac25639a54398b4032dd6a92e442e5a410267b27
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-30 18:15:07 -0300

    Add files via upload

---
+++ Condicional Ternário.js
@@ -0,0 +1,16 @@
+// Praticamente é um if em uma única linha
+//Calculando um frete
+let isMember = true;
+
+let shipping = isMember ? 2 : 10;
+    console.log(isMember ? "Você é Membro" : "Você não é Membro");
+    console.log("Frete: " + shipping);
+
+/* Condicional booleana
+: significa não 
+? significa sim
+*/
+
+
+
+
